700da9644c5582d2ac9e0a4f9c5d37081866e62e
Jessie <jessie.chen86828@gmail.com>
2026-04-20 19:44:32 +0800

    Created a working signup page - No secret key code yet

--- project/app.py
+++ project/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
+app.secret_key = '' #Will fix this code later - Whytothewhat
 
 
 @app.route('/')
@@ -22,6 +23,28 @@ def viewstats():
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+        
+        if password != confirm_password:
+            flash('Passwords do not match!', 'error')
+            return render_template('signup.html')
+        
+        if len(password) < 6:
+            flash('Password must be at least 6 characters long!', 'error')
+            return render_template('signup.html')
+        
+        #Store Users Here - Whytothewhat
+        flash('Account created successfully! Please log in.', 'success')
+        return redirect(url_for('login'))
+    
+    return render_template('signup.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
