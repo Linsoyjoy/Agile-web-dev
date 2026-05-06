e6202fdfcd1c2666e4b0f95b24bb51ec2d6fb9a6
Lindsay Wijaya <linwijaya06@gmail.com>
2026-05-06 16:26:19 +0800

    Revert "Reworked the structure of app.py"

--- project/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-default_db_path = 'sqlite:///' + os.path.join(basedir,"users.db")
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or default_db_path
-    SECRET_KEY = os.urandom(24)
